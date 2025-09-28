704c8cded08434dd7360612a7ed8bde5b88b9613
Иван Бу <ivanbu@MacBook-Pro.local>
2025-09-28 21:33:54 +0500

    Auto update (2025-09-28 21:33:54.160186)

--- inserted_code.py
+++ inserted_code.py
@@ -8,3 +8,24 @@
 # Мелкое улучшение 2025-09-28 21:26:58.997821
 # Автоген комментарий 2025-09-28 21:28:16.410132
 # Автоген комментарий 2025-09-28 21:31:13.148098
+
+# ===== вставка функции =====
+def show_phase_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show phase response of a filter
+# ===== конец вставки =====
+
+# ===== вставка функции =====
+def show_frequency_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show frequency response of a filter
+# ===== конец вставки =====
+
+# ===== вставка функции =====
+def process(self, sample: float) -> float:
+        """
+        Calculate y[n]
+# ===== конец вставки =====
+# Автоген комментарий 2025-09-28 21:33:54.130007
+
+# Мелкое улучшение 2025-09-28 21:33:54.130287

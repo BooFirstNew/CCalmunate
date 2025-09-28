0d4497fb7ee2622ec29e3e8149f36a2d7034e765
Иван Бу <ivanbu@MacBook-Pro.local>
2025-09-28 21:42:05 +0500

    Improve logging messages (2025-09-28 21:42:05.471022)

--- api_client.py
+++ api_client.py
@@ -1,1 +1,21 @@
 # Автоген комментарий 2025-09-28 21:38:41.615464
+
+# Мелкое улучшение 2025-09-28 21:42:05.450063
+
+# ===== вставка функции =====
+def show_phase_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show phase response of a filter
+# ===== конец вставки =====
+
+# ===== вставка функции =====
+def show_frequency_response(filter_type: FilterType, samplerate: int) -> None:
+    """
+    Show frequency response of a filter
+# ===== конец вставки =====
+
+# ===== вставка функции =====
+def process(self, sample: float) -> float:
+        """
+        Calculate y[n]
+# ===== конец вставки =====

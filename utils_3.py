575275e7b726d4a5788983a0ccf57c6771edd280
IvaBoo <urijsavelev314@gmail.com>
2025-10-01 16:19:20 +0000

    Improve error handling (2025-10-01 16:19:20.683144)

--- utils_3.py
+++ utils_3.py
@@ -1,3 +1,13 @@
 #  2025-10-01 11:06:59.704544
 
 # Мелкое улучшение 2025-10-01 11:07:14.667496
+
+def session(user: "User | None" = None):
+    # lazy import to avoid gevent monkey patching unless you actually use this fixture
+    from locust.clients import HttpSession
+
+
+def fastsession(user: "User | None" = None):
+    # lazy import to avoid gevent monkey patching unless you actually use this fixture
+    from locust.contrib.fasthttp import FastHttpSession
+

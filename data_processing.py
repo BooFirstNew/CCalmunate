9e702eadc8307366b404ed706d3cc4d68801c70d
root <root@r860215.u212232.tscom.kvm.ubuntu-20.04-amd64-r1>
2025-10-01 15:19:31 +0000

    Add comments and documentation (2025-10-01 15:19:31.663859)

--- data_processing.py
+++ data_processing.py
@@ -11,3 +11,18 @@ def new_downloader(option=None, downloader=None) -> JmDownloader:
     if option is None:
         option = JmModuleConfig.option_class().default()
 
+
+def session(user: "User | None" = None):
+    # lazy import to avoid gevent monkey patching unless you actually use this fixture
+    from locust.clients import HttpSession
+
+
+def fastsession(user: "User | None" = None):
+    # lazy import to avoid gevent monkey patching unless you actually use this fixture
+    from locust.contrib.fasthttp import FastHttpSession
+
+
+def pytest_configure(config):
+    global _config
+    _config = config
+
